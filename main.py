d4310b022c3e6b38b98ce2fb9a18af8bb7c537f8
Antoine <antoine.montier@sfr.fr>
2022-12-06 22:31:56 +0100

    added the mainloop

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from kivy.uix.widget import Widget
 from kivy.graphics import *
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.metrics import *
-from kivy.properties import ObjectProperty
+from kivy.metrics import dp
+from kivy.graphics.vertex_instructions import  *
+from kivy.properties import ObjectProperty, Clock
 from kivy.graphics.vertex import *
 
 kivy.require('1.9.0')
@@ -41,6 +42,46 @@ class Canvas2(Widget):
         if(x > self.width):
             x = dp(0)
         self.rect.pos = (x, y)
+
+class Canvas3(Widget):
+    def __init__(self, **kwargs):
+        self.fps = 60
+        self.vx = dp(5)
+        self.vy = dp(5)
+        self.ball_size = dp(50)
+        self.ball_x = self.width/2
+        self.ball_y = self.height/2
+        super(Canvas3, self).__init__(**kwargs)
+        with self.canvas:
+            self.ball = Ellipse(pos=(self.ball_x, self.ball_y), size=(self.ball_size, self.ball_size))
+
+        Clock.schedule_interval(self.update, 1/self.fps)#update every one second
+
+
+    def on_size(self, *args):
+        print("on_size : " + str(self.width) + ", "+ str(self.height))
+        self.ball.pos = self.width/2-self.ball_size/2, self.height/2-self.ball_size/2
+
+    def update(self, dt):#dt is delta time
+        x, y = self.ball.pos
+        if(x + self.ball.size[0] > self.width or x < 0):
+            self.vx *= -1
+        if(y+ self.ball.size[1] > self.height or y < 0):
+            self.vy *= -1
+        self.ball.pos = (x+self.vx, y+self.vy)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class MyApp(App):
     """
