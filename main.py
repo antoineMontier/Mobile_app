30a6a0f495816d444dac8e4d6aea2b166891864a
Antoine <antoine.montier@sfr.fr>
2022-12-06 21:53:34 +0100

    added moving and class control

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from kivy.uix.widget import Widget
 from kivy.graphics import *
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.metrics import *
 from kivy.properties import ObjectProperty
+from kivy.graphics.vertex import *
 
 kivy.require('1.9.0')
 
@@ -16,18 +18,32 @@ kivy.require('1.9.0')
 
 
 class MyScreen(Widget):
-    def __init__(self, **kwargs):
-        super(MyScreen, self).__init__(**kwargs)
+    pass
+    #def __init__(self, **kwargs):
+    #    super(MyScreen, self).__init__(**kwargs)
 
 
 
 class Canvas1(Widget):
     pass
     
+class Canvas2(Widget):
+    def __init__(self, **kwargs):
+        super(Canvas2, self).__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100, 100, 400, 500), width=5)
+            Color(0, 0.6, 0.4, 1)
+            self.rect = Rectangle(pos=(300, 400), size=(50, 50))
 
+    def redraw(self):
+        x, y = self.rect.pos
+        x += dp(50)
+        if(x > self.width):
+            x = dp(0)
+        self.rect.pos = (x, y)
 
 class MyApp(App):
-
+    """
     button_color = [1, 0, 0, 1]
 
     def __init__(self, **kwargs):
@@ -38,7 +54,8 @@ class MyApp(App):
         #Window.clearcolor = 9/255, 11/255, 10/255, 1#bg color
         #Window.size = (1080*0.3, 2400*0.3)
         return MyScreen()
-    
+    """
+    pass
 
 if __name__ == '__main__':
     MyApp().run()
